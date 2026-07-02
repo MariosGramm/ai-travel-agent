a32fec16032fc30b45474d4c8c9efed0ca97fa65
Marios Grammatopoulos <mgrammatopoulos@entersoft.gr>
2026-07-02 17:39:54 +0300

    Added env and cors configuration to Settings object

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,7 @@
-from pydantic_settings import BaseSettings
+import secrets
+from typing import Annotated, Literal
+from pydantic import AnyUrl, BeforeValidator
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 def parse_cors_origins(origins: str) -> list[str] | str:
     """
@@ -15,4 +18,18 @@ def parse_cors_origins(origins: str) -> list[str] | str:
     return [origin.strip() for origin in origins.split(",")]
 
 class Settings(BaseSettings):
-    
+    model_config = SettingsConfigDict(
+        env_file="../.env",     #one level up from the current file
+        env_ignore_empty = True,
+        extra = "ignore"
+    )
+
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # JWT token expires after 8 days
+    FRONTEND_HOST: str = "http://localhost:5173"
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+
+    BACKEND_CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors_origins)
+    ] = []
