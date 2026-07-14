c1afd0ad0a0520624a7b3a763c417dbcdd4c81bb
Marios Grammatopoulos <mgrammatopoulos@entersoft.gr>
2026-07-14 15:23:13 +0300

    Added update_user for user updates from the superuser

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,13 +1,34 @@
-from app.models import User, UserCreateDTO
+from app.models import User, UserCreateDTO, UserUpdateDTO
 from sqlmodel import Session
 
 
-def create_user(*, session: Session, userCreateDTO: UserCreateDTO ):
+def create_user(*, session:Session, user_creation_data:UserCreateDTO) -> User:
+    """
+    CRUD method for user creation.
+    """
     db_obj = User.model_validate(
-        userCreateDTO, update = {"hashed_password": get_password_hash(userCreateDTO.password)}
+        user_creation_data, update = {"hashed_password": get_password_hash(user_creation_data.password)}
     )
 
     session.add(db_obj)
     session.commit()
     session.refresh(db_obj)
     return db_obj
+
+def update_user(*, session:Session, user_update_data:UserUpdateDTO, user:User):
+    """
+    CRUD method for user update.
+    Used ONLY by the superuser.
+    """
+    user_data = user.model_dump(exclude_unset=True)
+    extra_data = {}
+
+    if "password" in user_data:
+        extra_data["hashed_password"] = get_password_hash(user_data.pop("password"))
+    
+    user.sqlmodel_update(user_data, update= extra_data)
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
