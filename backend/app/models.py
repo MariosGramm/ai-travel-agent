ce302fb30890e04817c3dba097fff1d36d463e19
Marios Grammatopoulos <mgrammatopoulos@entersoft.gr>
2026-07-20 18:51:36 +0300

    Added Token, Message, TokenPayload and NewPassword models in models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -425,3 +425,22 @@ class AccommodationPublicDTO(SQLModel):
     rating: float | None
     extra_info: str | None
 
+# Generic message
+class Message(SQLModel):
+    message: str
+
+
+# JSON payload containing access token
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+# Contents of JWT token
+class TokenPayload(SQLModel):
+    sub: str | None = None
+
+
+class NewPassword(SQLModel):
+    token: str
+    new_password: str = Field(min_length=8, max_length=128)
